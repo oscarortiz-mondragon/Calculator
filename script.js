7e16d6508c90e8582f9ffcbc02658f87f33fdc81
Oscar Ortiz-Mondragon <oscarortiz@utexas.edu>
2020-08-05 00:00:45 -0500

    needs a better tool than eval function

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-let screen = document.querySelector('p');
-let numbers = document.querySelector("#flex-container");
-let inputValues = Array.from(numbers.querySelectorAll("input"));
-let input = numbers.querySelectorAll("input");
 
-let values = inputValues.map(a => a.getAttribute('value'));
+// Clear
 
-screen.inn
+let clear = document.getElementById("clear")
+clear.onclick = function(){
+    para.textContent = ""
+    history.textContent= ""
+}
+
+let para = document.querySelector('p')
+let container = document.querySelector("#container")
+
+let div = container.getElementsByTagName('div')
+let history = document.getElementById("history")
+let equal = document.querySelector("#equal")
+
+
+equal.addEventListener('click',function(){
+    if(history.innerText == ""){
+        f = new Function(`para.textContent`, `return para.textContent`)
+        history.innerText = f(para.textContent)
+        para.textContent = ""
+    }
+   else if(history.innerText != ""){
+       history.innerText =  eval(history.innerText + para.textContent)
+       para.textContent = ""
+      
+  
+   }
+})
+
+for(let i = 0; i < div.length-1; i++)
+{
+  
+    div[i].addEventListener('click', function(){
+        if(para.innerText == "")
+    {
+    para.innerText = div[i].innerText
+    }
+        else if(para.innerText != "")
+    {
+            para.innerText = para.innerText + div[i].innerText
+    }
+   
+    })};
+
+let apple = "10 + 10"
+for(let i = 0; i < apple.length; i++){
+ cheese = parseInt(apple[i])}
